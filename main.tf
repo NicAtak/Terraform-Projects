7d38fc9d24e148c8620753dd10bd62d5c6ddb0e6
NicAtak <ndf.mendoza@gmail.com>
2023-03-08 16:27:43 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,150 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.57.1"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+
+
+# 1. Create VPC
+
+resource "aws_vpc" "project-vpc" {
+  cidr_block       = "10.0.0.0/16"
+    tags = {
+    Name = "project"
+  }
+}
+# 2. Creat Internet Gateway
+resource "aws_internet_gateway" "project-gw" {
+  vpc_id = aws_vpc.project-vpc.id
+
+  tags = {
+    Name = "project"
+  }
+}
+# 3. Create custom Route Table
+resource "aws_route_table" "project-rt" {
+  vpc_id = aws_vpc.project-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.project-gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.project-gw.id
+  }
+
+  tags = {
+    Name = "project"
+  }
+}
+# 4. Create a Subnet
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.project-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "project-subnet"
+  }
+}
+
+# 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.project-rt.id
+}
+
+# 6. Creat Security Group to allow port 22, 80, 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.project-vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# 7. Creat a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "webserver-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+  }
+
+# 8. Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "project-eip" {
+  vpc      = true
+  network_interface = aws_network_interface.webserver-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.project-gw]
+}
+
+output "server_public_ip" {
+    value = aws_eip.project-eip.public_ip
+}
+
+# 9. Creat Ubuntu server and install/enable apache2
+resource "aws_instance" "web-server-instance" {
+    ami = "ami-0735c191cf914754d"
+    instance_type = "t2.micro"
+    availability_zone = "us-west-2a"
+    key_name = "main-key"
+
+    network_interface {
+        device_index = 0
+        network_interface_id = aws_network_interface.webserver-nic.id
+    }
+
+    user_data = <<-EOF
+            #!/bin/bash
+            sudo apt update -y
+            sudo apt install apache1 -y
+            duo systemctl start apache2
+            sudo bash -c "echo your very first webserver > /var/www/html/index.html"
+            EOF
+    tags = {
+        Name = "web-server"
+    }
+
+}
+
+
