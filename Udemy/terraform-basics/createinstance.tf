209bf80d71915ec0ddaa3753ab29c2e088689fb8
NicAtak <ndf.mendoza@gmail.com>
2023-03-28 15:01:59 -0700

    four instances

--- Udemy/terraform-basics/createinstance.tf
+++ Udemy/terraform-basics/createinstance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "udemy_instance" {
-  count         = 3
+  count         = 4
   ami           = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "threedemoinstance"
+    Name = "demoinstance-${count.index}"
   }
 }
