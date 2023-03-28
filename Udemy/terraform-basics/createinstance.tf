1e6a95c7457f37c8486c785438e179388208f1fc
NicAtak <ndf.mendoza@gmail.com>
2023-03-28 14:25:38 -0700

    threeinstancedemo

--- Udemy/terraform-basics/createinstance.tf
+++ Udemy/terraform-basics/createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "udemy_instance" {
+  count         = 3
   ami           = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
+
+  tags{
+    Name = "threedemoinstance"
+  }
 }
