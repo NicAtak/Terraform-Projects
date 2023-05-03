0ec90656614604fc4ca660a074339844114e370f
NicAtak <ndf.mendoza@gmail.com>
2023-05-03 15:50:48 -0700

    updated the provider source and version of terraform

--- Udemy/building-blocks/provider.tf
+++ Udemy/building-blocks/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
