14c72b914af9b83c2dec504417d783781c8f8b9e
Nicole M <91444149+NicAtak@users.noreply.github.com>
2023-08-11 16:08:28 -0700

    Delete Udemy/droplet-basics directory

--- Udemy/droplet-basics/createinstance.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    access_key = "AKIAXQCRRJPDWMRQPJGB"
-    secret_key = "SECRET KEY"
-    region     = "us-west-2"
-
-resource "aws_instance" "MyFirstInstance"
-ami            = "ami-0688ba7eeeeefe3cd"
-instance_type  = "t2.micro"
-}
